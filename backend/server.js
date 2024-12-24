18e2350fac08f3b688abf341a71e331a6ed255d7
Thepowerfulmark <mark_ediev@bk.ru>
2024-12-24 03:20:08 +0300

    next_commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Путь к папке frontend, которая находится на уровне выше
 const frontendPath = path.join(__dirname, '..', 'frontend');
